8ebb7f54f6d64580d714b74986e7c4c9a2f8770a
saku-1101 <sakupi1101@outlook.jp>
2025-05-17 15:04:25 +0900

    chore: test

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -1,9 +1,10 @@
 import { assertEquals } from "jsr:@std/assert";
-import { type Blog, createSakupi01McpServer } from "../src/mod.ts";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
-import { runServer } from "../index.ts";
 import mockItems from "./__fixtures__/blogs.json" with { type: "json" };
+import { createSakupi01McpServer } from "../src/server.ts";
+import type { Blog } from "../src/types.ts";
+import { runServer } from "../src/mod.ts";
 
 const createConnection = async (url: string) => {
   const client = new Client({
@@ -57,26 +58,38 @@ Deno.test("should search blogs with query", async () => {
       },
     });
 
-    assertEquals(response, {
-      content: [
-        {
-          type: "text",
-          text: "[\n" +
-            "  {\n" +
-            '    "title": "🎄Open UI Advent Calendar: Day 11 / Customizable Select Element Ep.9",\n' +
-            '    "pubDate": "2024-12-11T00:00:00.000Z",\n' +
-            '    "description": "Customizable Select Elementの関連仕様:  `appearance: base-select;` - `::picker-icon`のデフォルトスタイルはどうやって決まったのか",\n' +
-            '    "link": "https://blog.sakupi01.com/dev/articles/2024-openui-advent-11/",\n' +
-            '    "content": "Test Test",\n' +
-            '    "tags": [\n' +
-            '      "openui",\n' +
-            '      "advent calendar"\n' +
-            "    ]\n" +
-            "  }\n" +
-            "]",
-        },
-      ],
-    });
+    assertEquals(response.content, [
+  {
+    type: "text",
+    text: "{\n" +
+      '  "total": 1,\n' +
+      '  "offset": 0,\n' +
+      '  "limit": 2,\n' +
+      '  "order": "desc",\n' +
+      '  "query": {\n' +
+      '    "original": "Test",\n' +
+      '    "keywords": [\n' +
+      '      "test"\n' +
+      "    ],\n" +
+      '    "exactPhrases": []\n' +
+      "  },\n" +
+      '  "results": [\n' +
+      "    {\n" +
+      '      "title": "🎄Open UI Advent Calendar: Day 11 / Customizable Select Element Ep.9",\n' +
+      '      "pubDate": "2024-12-11T00:00:00.000Z",\n' +
+      '      "description": "Customizable Select Elementの関連仕様:  `appearance: base-select;` - `::picker-icon`のデフォルトスタイルはどうやって決まったのか",\n' +
+      '      "link": "https://blog.sakupi01.com/dev/articles/2024-openui-advent-11/",\n' +
+      '      "content": "Test Test",\n' +
+      '      "tags": [\n' +
+      '        "openui",\n' +
+      '        "advent calendar"\n' +
+      "      ],\n" +
+      '      "score": 2\n' +
+      "    }\n" +
+      "  ]\n" +
+      "}"
+  }
+]);
   } finally {
     client.close();
     await cleanup();
